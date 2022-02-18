83a486d678b3936fae3f30b9c29e3bca764b141b
Chen ChiaChi <chenchiachi.ccc@gmail.com>
2022-02-18 15:04:45 +0100

    feat: add api-auth middleware

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -6,12 +6,13 @@ const admin = require('./modules/admin')
 
 const restController = require('../../controllers/apis/restaurant-controller')
 const userController = require('../../controllers/apis/user-controller')
+const { authenticated, authenticatedAdmin } = require('../../middleware/api-auth')
 const { apiErrorHandler } = require('../../middleware/error-handler')
 
-router.use('/admin', admin)
+router.use('/admin', authenticated, authenticatedAdmin, admin)
 
 router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn) // 關掉建立session
-router.get('/restaurants', restController.getRestaurants)
+router.get('/restaurants', authenticated, restController.getRestaurants)
 router.use('/', apiErrorHandler)
 
 module.exports = router
